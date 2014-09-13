a161a53c680723b27a7a3c18a6be07db2b6e17c7
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-09-13 00:35:15 -0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/axxion/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/axxion/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_d2lte
+PRODUCT_NAME := axxion_d2lte
